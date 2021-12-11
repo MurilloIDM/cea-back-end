8c635434513eef10aef50da47353d05b5e51207d
MurilloIDM <murillo.idem@gmail.com>
2021-12-10 21:45:55 -0300

    Initial commit

---
+++ src/main/java/com/cea/CeaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.cea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cea/CeaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
