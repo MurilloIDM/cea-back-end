e15dd938753e48f4155142eb44f6a661e124c35e
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-01-25 21:23:57 -0300

    Fix: alteração e criação de métodos do lead

--- src/main/java/com/cea/controller/LeadController.java
+++ src/main/java/com/cea/controller/LeadController.java
@@ -2,7 +2,9 @@ package com.cea.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
+import org.hibernate.type.BinaryType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,6 +57,12 @@ public class LeadController {
 		Pageable pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		Page<Lead> leads = this.leadService.findAllByPage(pageRequest);
 		return ResponseEntity.ok().body(leads);
+	}
+	/*check deviceId*/
+	@GetMapping("/{deviceId}")
+	public boolean findByDeviceId (@PathVariable UUID deviceId){
+		boolean leadExist = leadService.findByDeviceId(deviceId);
+		return leadExist;
 	}
 
 }
