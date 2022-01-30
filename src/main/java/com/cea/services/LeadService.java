af771d45459caa1fe57da875a343e7cd7aebf3d5
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-01-29 23:17:53 -0300

    update versão prod

--- src/main/java/com/cea/services/LeadService.java
+++ src/main/java/com/cea/services/LeadService.java
@@ -3,7 +3,6 @@ package com.cea.services;
 import java.util.List;
 import java.util.UUID;
 
-import org.hibernate.type.BinaryType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
