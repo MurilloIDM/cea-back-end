5b43bc7c3d6fcb19de476c154f9fd2b3c77303f0
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-02-18 19:57:45 -0300

    Update: configuração do cors

--- src/main/java/com/cea/CeaBackEndApplication.java
+++ src/main/java/com/cea/CeaBackEndApplication.java
@@ -6,9 +6,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CeaBackEndApplication {
+public class CeaBackEndApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeaBackEndApplication.class, args);
@@ -17,6 +20,17 @@ public class CeaBackEndApplication {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
