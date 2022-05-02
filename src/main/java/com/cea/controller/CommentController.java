be6cb51cbe87bd7537b98285daa5c044b023b45a
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-05-02 19:37:25 -0300

    New: implementação de serviço para adicionar comentário

--- src/main/java/com/cea/controller/CommentController.java
+++ src/main/java/com/cea/controller/CommentController.java
@@ -1,15 +1,28 @@
 package com.cea.controller;
 
+import com.cea.dto.comment.CommentDTO;
 import com.cea.services.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
-public class CommentController {
+public class CommentController extends BasicController {
 
     private final CommentService commentService;
+
+    @PostMapping("/create")
+    public ResponseEntity addComment(@RequestBody @Valid CommentDTO payload) {
+        this.commentService.addComment(payload);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
