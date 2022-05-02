a4f6a787456eca5c65a69f0999c7859645feb908
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-05-02 20:22:11 -0300

    New: adição serviço para criação de resposta de comentário

--- src/main/java/com/cea/services/CommentService.java
+++ src/main/java/com/cea/services/CommentService.java
@@ -1,12 +1,9 @@
 package com.cea.services;
 
 import com.cea.dto.comment.CommentDTO;
-import com.cea.models.Comment;
-import com.cea.models.CommentReply;
-import com.cea.models.ExclusivePost;
-import com.cea.models.Student;
-import com.cea.repository.CommentRepository;
-import com.cea.repository.ExclusivePostRepository;
+import com.cea.dto.comment.CommentReplyDTO;
+import com.cea.models.*;
+import com.cea.repository.*;
 import com.cea.utils.LocalDateTimeUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,7 +19,10 @@ import java.util.UUID;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final CommentReplyRepository commentReplyRepository;
     private final ExclusivePostRepository exclusivePostRepository;
+    private final StudentRepository studentRepository;
+    private final AdministratorRepository administratorRepository;
     private final StudentService studentService;
     private final LocalDateTimeUtils localDateTimeUtils;
 
@@ -58,6 +58,59 @@ public class CommentService {
         comment.setExclusivePost(exclusivePost.get());
 
         this.commentRepository.save(comment);
+    }
+
+    public void addCommentReply(CommentReplyDTO payload) {
+        boolean isAdmin = false;
+        boolean isStudent = true;
+
+        Optional<Student> student = null;
+        Optional<Administrator> administrator = null;
+
+        UUID userId = payload.getUserId();
+        UUID commentId = payload.getCommentId();
+
+        student = this.studentRepository.findById(userId);
+
+        if (student.isEmpty()) {
+            administrator = this.administratorRepository.findById(userId);
+
+            if (administrator.isEmpty()) {
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Estudante/Administrator não encontrado!");
+            }
+
+            isAdmin = true;
+            isStudent = false;
+        }
+
+        Optional<Comment> comment = this.commentRepository.findById(commentId);
+
+        if (comment.isEmpty()) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Comentário não encontrado!");
+        }
+
+        ExclusivePost exclusivePost = comment.get().getExclusivePost();
+
+        if (!exclusivePost.isStatus() || exclusivePost.isFiled()) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Conteúdo inválido para inserir comentário!");
+        }
+
+        String text = payload.getText();
+        LocalDateTime dateNow = this.localDateTimeUtils.dateNow();
+
+        CommentReply commentReply = new CommentReply();
+        commentReply.setText(text);
+        commentReply.setStatus(true);
+        commentReply.setCreatedAt(dateNow);
+        commentReply.setComment(comment.get());
+
+        if (isAdmin) {
+            commentReply.setAdministrator(administrator.get());
+        } else {
+            commentReply.setStudent(student.get());
+        }
+
+        this.commentReplyRepository.save(commentReply);
     }
 
 }
